c666d89908bc09c7f87878c2221abcf52afb74d0
TejasJainX <jaintejas325@gmail.com>
2026-06-11 04:18:39 +0530

    Git Notes

---
+++ GIT_NOTES.py
@@ -0,0 +1,73 @@
+''' 
+GIT
+
+cd = change directory (means going to a folder in a folder)
+command will be cd {foler_name}
+
+use clear in terminal to clear the chat
+
+ls = list files in the folder
+
+ls -a (this also shows hidden files (a means all))
+
+When you're working on Git stuff:
+
+Open Terminal in VS Code
+Click the ▼ next to the +
+Click Git Bash 
+
+use git init to convert the parent folder into a repository
+
+use git status to check the status of the file if its modified or like untracked 
+then add them by using git add {File name} and finally commit to save changes in
+your local device
+
+to put them into the github to save the changes use the push cmd like
+git push origin main (upload local repo to remote repo)
+
+local (your device)
+remote(github)
+
+(pro tip use git add . to make everything saved)
+
+commit means changes 
+
+use bash terminal mainly for working with git in vs code 
+
+use cd .. to leave a repo/folder
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
